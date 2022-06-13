53046c1297153770899388f2449528d5d4792f6b
Ben Harris <bjh21@bjh21.me.uk>
2022-06-14 00:27:42 +0100

    Re-organise into a single coherent Python script
    I think we'll end up with a fairly complex bot script for updating
    galleries and a bunch of back-ends that can fetch file lists from
    different sources.  Currently the front-end is pretty minimal and
    the back-ends load from Taginfo and Overpass.

---
+++ osm.py
@@ -0,0 +1,44 @@
+import re
+from urllib.parse import urlencode
+
+def from_taginfo(session):
+    r = session.get("https://taginfo.openstreetmap.org/api/4/key/values"
+                    "?key=wikimedia_commons")
+    r.raise_for_status()
+    j = r.json()
+    files = {}
+    for v in j["data"]:
+        if re.match("^File:", v["value"], re.IGNORECASE):
+            params = urlencode(dict(key="wikimedia_commons", value=v["value"]))
+            tiurl = "https://https://taginfo.openstreetmap.org/tags/?" + params
+            files[v['value']] = f"[{tiurl} {v['count']} use(s)]"
+    return files
+
+oplq = """
+[out:json];
+// gather results
+(
+  node["wikimedia_commons"~"(^|;)File:",i];
+  way["wikimedia_commons"~"(^|;)^File:",i];
+  relation["wikimedia_commons"~"(^|;)File:",i];
+);
+// print results
+out tags qt;
+"""
+
+def from_overpass(session):
+    r = session.get("https://overpass-api.de/api/interpreter",
+                    params={'data': oplq})
+    r.raise_for_status()
+    j = r.json()
+    files = {}
+
+    for e in j["elements"]:
+        wc = e.get("tags", {}).get("wikimedia_commons")
+        if wc != None:
+            osm_type = e["type"]
+            osm_id = e["id"]
+            files[wc] = (f"[https://www.openstreetmap.org/{osm_type}/{osm_id} "
+                         f"{osm_type} {osm_id}]")
+    return files
+
