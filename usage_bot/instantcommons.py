538ff66b8c85a309397d39903e64a9f6955c6f15
Ben Harris <bjh21@bjh21.me.uk>
2022-06-21 09:59:12 +0100

    Back-end to fetch data from a wiki using InstantCommons

---
+++ usage_bot/instantcommons.py
@@ -0,0 +1,14 @@
+import pywikibot.data.api as api
+
+def from_instantcommons(site):
+    # Logically, we could use a PageGenerator here, but we only want a
+    # couple of values and want to minimise load on the target.
+    g = api.QueryGenerator(site = site, parameters=dict(
+        generator='allfileusages', gafunique=True,
+        prop='imageinfo|info', iiprop='', inprop='url'))
+    files = {}
+    for f in g:
+        if f['imagerepository'] == 'wikimediacommons':
+            files[f['title']] = (
+                f"[{f['fullurl']} {f['title']}")
+    return files
