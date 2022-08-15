3a919b95d3720086a82c900c29a9eed8b04ec077
Ben Harris <bjh21@bjh21.me.uk>
2022-08-15 18:00:05 +0100

    Switch from_instantcommons to being a class

--- usage_bot/instantcommons.py
+++ usage_bot/instantcommons.py
@@ -1,14 +1,13 @@
 import pywikibot.data.api as api
+import pywikibot
 
-def from_instantcommons(site):
-    # Logically, we could use a PageGenerator here, but we only want a
-    # couple of values and want to minimise load on the target.
-    g = api.QueryGenerator(site = site, parameters=dict(
-        generator='allfileusages', gafunique=True,
-        prop='imageinfo|info', iiprop='', inprop='url'))
-    files = {}
-    for f in g:
-        if f['imagerepository'] == 'wikimediacommons':
-            files[f['title']] = (
-                f"[{f['fullurl']} {f['title']}]")
-    return files
+class from_instantcommons(dict):
+    def __init__(self, site):
+        # Logically, we could use a PageGenerator here, but we only want a
+        # couple of values and want to minimise load on the target.
+        g = api.QueryGenerator(site = site, parameters=dict(
+            generator='allfileusages', gafunique=True,
+            prop='imageinfo|info', iiprop='', inprop='url'))
+        for f in g:
+            if f['imagerepository'] == 'wikimediacommons':
+                self[f['title']] = f"[{f['fullurl']} {f['title']}]"
