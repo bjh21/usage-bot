0909f1100cbf29c41491deb08e68138eb884aab2
Ben Harris <bjh21@bjh21.me.uk>
2022-08-16 19:27:39 +0100

    Use interwiki links where possible for InstantCommons users
    This results in substantially smaller pages, and feels a bit more
    elegant.  It can't work in all cases, though, because InstantCommons
    users don't necessarily appear in Commons' interwiki map.
    
    Pywikibot has native support for generating interwiki links, but it
    doesn't work in the general case.  We might be able to construct the
    links automatically by reversing the interwiki map, but just taking an
    interwiki prefix as part of the configuration for a user is a lot
    simpler.  It's not like we need to track rapid updates to the interwiki
    map anyway.

--- usage_bot/instantcommons.py
+++ usage_bot/instantcommons.py
@@ -2,12 +2,29 @@ import pywikibot.data.api as api
 import pywikibot
 
 class from_instantcommons(dict):
-    def __init__(self, site):
+    def __init__(self, site, iwprefix=None):
         # Logically, we could use a PageGenerator here, but we only want a
         # couple of values and want to minimise load on the target.
-        g = api.QueryGenerator(site = site, parameters=dict(
+        parameters = dict(
             generator='allfileusages', gafunique=True,
-            prop='imageinfo|info', iiprop='', inprop='url'))
+            prop='imageinfo', iiprop='')
+        if iwprefix == None:
+            parameters['prop'] += '|info'
+            parameters['inprop'] = 'url'
+        g = api.QueryGenerator(site=site, parameters=parameters)
         for f in g:
             if f['imagerepository'] == 'wikimediacommons':
-                self[f['title']] = f"[{f['fullurl']} {f['title']}]"
+                if iwprefix == None:
+                    self[f['title']] = f"[{f['fullurl']} {f['title']}]"
+                else:
+                    # pywikibot.Link theoretically supports generating
+                    # interwiki links between arbitrary Sites, but in
+                    # practice it's not reliable.  For instance for a
+                    # link from Commons to Wikitech it generates
+                    # [[wikitech:en:File:Example.png]] which actually
+                    # ends us up at English Wikipedia.  So we just
+                    # allow the caller to specify an interwiki prefix,
+                    # which isn't very much additional hassle and is
+                    # much simpler than working it out properly from
+                    # the interwiki map.
+                    self[f['title']] = f"[[{iwprefix}:{f['title']}]]"
