7502fe34161988f00ed2cbe825610df5eff5e117
Ben Harris <bjh21@bjh21.me.uk>
2022-08-18 22:54:42 +0100

    Remove namespace prefix from foreign file titles
    The file titles returned by the API on other wikis are the localised
    ones for those wikis, which means that they might not work on
    Commons.  Since <gallery> doesn't require the namespace prefix, I
    simply remove the namespace prefix from all the file titles imported
    from foreign wikis.
    
    I haven't done this at a higher level because that would involve
    questions of what to do when a back-end returns files that differ
    only in the namespace prefix (case and/or spacing).  InstantCommons
    can't do this, but OpenStreetMap can.

--- usage_bot/instantcommons.py
+++ usage_bot/instantcommons.py
@@ -13,9 +13,24 @@ class from_instantcommons(dict):
             parameters['inprop'] = 'url'
         g = api.QueryGenerator(site=site, parameters=parameters)
         for f in g:
+            # Properly, we should query meta=filerepoinfo to find the
+            # name of Commons on this wiki, but InstantCommons seems
+            # to always use "wikimediacommons" so for now we just spot
+            # that.
             if f['imagerepository'] == 'wikimediacommons':
+                # Other wikis might have their own namespace prefix in
+                # place of "File:", but those won't work on Commons.
+                # Assume that everything up to the first colon is the
+                # namespace prefix.
+                commons_title = f['title'].split(':', 1)[1]
+                # We could stick "File:" on the front for Commons, but
+                # <gallery> works perfectly well without it and that
+                # saves 5 KB per gallery.  At least, until someone
+                # uploads a file to Commons whose name begins "File:",
+                # but a search suggests that this has only every
+                # happened once.
                 if iwprefix == None:
-                    self[f['title']] = f"[{f['fullurl']} {f['title']}]"
+                    self[commons_title] = f"[{f['fullurl']} {f['title']}]"
                 else:
                     # pywikibot.Link theoretically supports generating
                     # interwiki links between arbitrary Sites, but in
@@ -27,4 +42,4 @@ class from_instantcommons(dict):
                     # which isn't very much additional hassle and is
                     # much simpler than working it out properly from
                     # the interwiki map.
-                    self[f['title']] = f"[[{iwprefix}:{f['title']}]]"
+                    self[commons_title] = f"[[{iwprefix}:{f['title']}]]"
