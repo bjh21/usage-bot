14b0729342696cb52b9128bf4542db5cdcacff6b
Ben Harris <bjh21@bjh21.me.uk>
2022-08-24 09:49:20 +0100

    Factor out common code between usage-bot and usage-demo
    This introduces a common middle layer between the front-ends (usage-bot
    and usage-demo) and the back-ends (OSM and InstantCommons).  This lives
    in the usage_bot module.
    
    Some of this was accidentally committed in 0344b295.

---
+++ usage_bot/__init__.py
@@ -0,0 +1,22 @@
+import pywikibot
+
+from usage_bot.instantcommons import from_instantcommons
+from usage_bot.osm import from_taginfo, from_overpass
+
+def filter_files(files):
+    # Filter out alleged filenames with impossible characters
+    for title in list(files.keys()):
+        if '|' in title:
+            del files[title]
+
+def from_args(args):
+    if '-osm' in args:
+        files = from_taginfo()
+    elif '-osmwiki' in args:
+        files = from_instantcommons(pywikibot.Site("osm:en"),
+                                    iwprefix="osmwiki")
+    elif '-wikitech' in args:
+        files = from_instantcommons(pywikibot.Site("wikitech:en"),
+                                    iwprefix="wikitech")
+    filter_files(files)
+    return files
