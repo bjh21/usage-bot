6fa0f6b558f5fa98063ad5c9eb2b4544dd32f5f7
Ben Harris <bjh21@bjh21.me.uk>
2022-09-11 00:17:08 +0100

    Canonicalise whitespace and bidi overrides in filenames harder
    All the space characters that are handled by MediaWiki are now
    flattened to single spaces, leading and trailing spaces are stripped,
    and bidi control characters are removed.

--- usage_bot/util.py
+++ usage_bot/util.py
@@ -4,10 +4,17 @@ from urllib.parse import unquote
 
 def canonicalise_name(name):
     # Produce the canonical form of a name for use in galleries and as
-    # a key.  Galleries don't need the namespace prefix, so we strip
-    # it off.
-    name = unquote(name).replace("_", " ")
-    name = re.sub(r"^File:\s*", "", name, flags=re.IGNORECASE)
+    # a key.  Broadly equivalent to part of the splitTitleString
+    # function in MediaWiki.
+    name = unquote(name)
+    # Strip bidi overrides.
+    name = re.sub(r"[\u200E\u200F\u202A-\u202E]+", "", name)
+    # Normalise spaces.
+    name = re.sub(r"[ _\xA0\u1680\u180E\u2000-\u200A\u2028\u2029\u202F"
+                  r"\u205F\u3000]+", " ", name)
+    name = name.strip(" ")
+    # Galleries don't need the namespace prefix, so we strip it off.
+    name = re.sub(r"^File: *", "", name, flags=re.IGNORECASE)
     if len(name) > 0:
         # This is not the proper way to capitalise a string, since it
         # messes up characters with a titlecase form.  It seems to
